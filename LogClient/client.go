b3adbfe7b31a300152b85dd1b37057242bee65f4
Esan Samuel <esansamuel555@gmail.com>
2026-01-22 13:09:26 +0100

    commits

--- LogClient/client.go
+++ LogClient/client.go
@@ -1,18 +1,13 @@
 package logClient
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net"
 	"runtime"
 	"time"
 
-	"github.com/EsanSamuel/sensory/db"
-	"github.com/EsanSamuel/sensory/helpers"
 	"github.com/EsanSamuel/sensory/models"
-	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type Client struct {
@@ -25,36 +20,17 @@ type Client struct {
 	noOp      bool // flag to mark dummy client
 }
 
-func New(apikey, addr string) (*Client, error) {
+func New(apikey, projectName, addr string) (*Client, error) {
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	var project models.Project
-	err = db.ProjectCollection.FindOne(ctx, bson.M{"api_key": apikey}).Decode(&project)
-	if err != nil {
-		conn.Close()
-		log.Println("Invalid api_key")
-		return nil, err
-	}
-
 	client := &Client{
-		conn:      conn,
-		Project:   project.ProjectName,
-		Service:   project.Service,
-		ApiKey:    apikey,
-		ProjectId: project.ProjectID,
-		UserId:    project.UserID,
-		noOp:      false,
-	}
-
-	err = helpers.SaveProjectMeta(client.Project, client.ProjectId, client.UserId)
-	if err != nil {
-		log.Println(err)
+		conn:    conn,
+		Project: projectName,
+		ApiKey:  apikey,
+		noOp:    false,
 	}
 
 	return client, nil
--- config/logger.go
+++ config/logger.go
@@ -1,7 +1,1 @@
 package config
-
-import (
-	logClient "github.com/EsanSamuel/sensory/LogClient"
-)
-
-var Logger, _ = logClient.New("7ed173cfa5dca9a4be50d9c6b6717b7a3b151839b5042ad592a59265fd8b68cf", ":9000")
